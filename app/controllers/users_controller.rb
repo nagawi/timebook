2e5ac6b405f3ed9b966a5061be3daa5a8d7e54c7
長井 学 <nagaimanabu@nagai-gaku-no-MacBook-Air-3.local>
2016-03-03 22:40:47 +0900

    登録画面の作成

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,30 @@
+class UsersController < ApplicationController
+  def index
+
+    @users = User.all
+
+  end
+
+  def new
+
+    @user = User.new
+
+  end
+
+  def create
+
+    @user = User.new(user_params)
+
+    @user.save!
+    redirect_to  :action => 'index',  notice: 'user was successfully updated.' 
+  rescue => e
+    render :'new'
+
+  end
+
+  def user_params
+
+    params.require(:user).permit(:name,:email)
+
+  end
+end
