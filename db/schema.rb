003efea5c0441cf2587e9bb3e86505913f09482a
長井 学 <nagaimanabu@nagai-gaku-no-MacBook-Air-3.local>
2016-03-09 23:55:23 +0900

    ログイン画面作成（途中）

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160306093729) do
+ActiveRecord::Schema.define(version: 20160309122210) do
 
   create_table "postings", force: :cascade do |t|
     t.string   "name",          limit: 255
@@ -23,10 +23,11 @@ ActiveRecord::Schema.define(version: 20160306093729) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "name",       limit: 255
-    t.string   "email",      limit: 255
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
+    t.string   "name",            limit: 255
+    t.string   "email",           limit: 255
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+    t.string   "password_digest", limit: 255
   end
 
 end
